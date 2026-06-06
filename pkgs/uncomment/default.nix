14e8a9bf379fc236208b7df82f7792fe5cf8bba6
hakan-demirli <ehdemirli@proton.me>
2026-06-06 21:25:54 +0200

    chore(uncomment): bump

--- pkgs/uncomment/default.nix
+++ pkgs/uncomment/default.nix
@@ -4,15 +4,15 @@
 
 pkgs.rustPlatform.buildRustPackage {
   pname = "uncomment";
-  version = "3.0.2";
+  version = "3.0.3";
 
   src = pkgs.fetchFromGitHub {
     owner = "Goldziher";
     repo = "uncomment";
-    rev = "3ee8bb325a17b63f16d682219ee5b8ab123da767";
-    hash = "sha256-AzwOBDlAylF5gLiKOot1iBhgFbWpm8bKl+1rAGUs7Zg=";
+    rev = "a2a898c556a503922017cffa9a4f96ca46ef098b";
+    hash = "sha256-A76V1XA0aPsBGBMDVfjyOHLcf/6HOAK8AtXvqOVSw7E=";
   };
 
-  cargoHash = "sha256-ccJ1jhNfqrZSEf42nZyzLIIO7aD74LCNHalePRCDgZQ=";
+  cargoHash = "sha256-vwBIiwN2SMkIeEQBYc2BoC04mHxtfs4oKM6LKa1qdUA=";
   doCheck = false;
 }
